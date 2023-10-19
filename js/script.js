9a88b2f3699f504518c3293ec5018fc814a7c909
silasoyibs <silasoyibs@gmail.com>
2023-10-19 18:42:10 +0100

    setting abuja default values

--- js/script.js
+++ js/script.js
@@ -6,12 +6,42 @@ const countryCity = document.querySelector(".city");
 const windSpeed = document.querySelector(".windspeed");
 const humidityPercentage = document.querySelector(".humidity");
 const  weatherTypeimg = document.querySelector(".weather-illustration-container-image");
+// resetting values
+weatherTemp.innerHTML = "--- ";
+countryCity.innerHTML = "--- ";
+windSpeed.innerHTML = "---";
+humidityPercentage.innerHTML="---"
+// Functions
+function reset(weatherTemp,countryCity, windSpeed,humidityPercentage,weatherObj){
+  weatherTemp.innerHTML = `${Math.round(weatherObj.temp)}°`;
+  countryCity.innerHTML = weatherObj.city;
+  windSpeed.innerHTML = `${weatherObj.windSpeed} km/h`;
+  humidityPercentage.innerHTML = `${weatherObj.humidity}%`;
 
+}
+function settingImage(weatherObj, weatherTypeimg){
+  if(weatherObj.weatherType === "Clouds"){
+    weatherTypeimg.src="img/clouds.png"
+  }
+  else if(weatherObj.weatherType === "Rain"){
+    weatherTypeimg.src="img/rain.png"
+  }
+  else if(weatherObj.weatherType === "snow"){
+    weatherTypeimg.src="img/snow.png"
+  }
+  else if(weatherObj.weatherType === "Clear"){
+    weatherTypeimg.src="img/clear.png"
+  }
+}
+
+
+// click Event
+let country = 'Abuja';
+const apiKey = "3c6792811bf7f8cddf25779aa975ee61";
 searchBtn.addEventListener("click", searchBtnClick);
 function searchBtnClick() {
-  let country = searchInput.value;
+  country = searchInput.value;
   country = country[0].toUpperCase() + country.slice(1);
-  const apiKey = "3c6792811bf7f8cddf25779aa975ee61";
   async function getWeatherSearch() {
     try {
       const response = await fetch(
@@ -30,26 +60,9 @@ function searchBtnClick() {
         windSpeed: weatherData.wind.speed,
         weatherType: weatherData.weather[0].main,
       };
-
-      // setting static Html value
-      weatherTemp.innerHTML = `${Math.round(weatherObj.temp)}°`;
-      countryCity.innerHTML = weatherObj.city;
-      windSpeed.innerHTML = `${weatherObj.windSpeed} km/h`;
-      humidityPercentage.innerHTML = `${weatherObj.humidity}%`;
-
+      reset(weatherTemp,countryCity, windSpeed,humidityPercentage,weatherObj);
       // setting image
-      if(weatherObj.weatherType === "Clouds"){
-        weatherTypeimg.src="img/clouds.png"
-      }
-      else if(weatherObj.weatherType === "Rain"){
-        weatherTypeimg.src="img/rain.png"
-      }
-      else if(weatherObj.weatherType === "snow"){
-        weatherTypeimg.src="img/snow.png"
-      }
-      else if(weatherObj.weatherType === "Clear"){
-        weatherTypeimg.src="img/clear.png"
-      }
+      settingImage(weatherObj, weatherTypeimg);
       // make input search empty
       searchInput.value = "";
     } catch (error) {
@@ -58,3 +71,34 @@ function searchBtnClick() {
   }
   getWeatherSearch();
 }
+// Default setting
+
+  async function getWeatherSearch() {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${apiKey}`
+      );
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("city not found");
+      }
+      const weatherData = await response.json();
+      console.log(weatherData);
+      const weatherObj = {
+        city: weatherData.name,
+        temp: weatherData.main.temp,
+        humidity: weatherData.main.humidity,
+        windSpeed: weatherData.wind.speed,
+        weatherType: weatherData.weather[0].main,
+      };
+      // setting static Html value
+      reset(weatherTemp,countryCity, windSpeed,humidityPercentage,weatherObj);
+      // setting image
+      settingImage(weatherObj, weatherTypeimg);
+      // make input search empty
+      searchInput.value = "";
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  getWeatherSearch();
